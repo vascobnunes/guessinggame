81a78b2788adb813c8368937679e0911269c79db
vascobnunes <vascobnunes@gmail.com>
2019-04-22 18:34:36 +0200

    first working try

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,24 @@
-cd
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+count_of_files=$(ls | wc -l)
+action="Please try again!"
+
+while [[ $count_of_files -ne $response ]]
+do
+	echo "Type how many files you guess are in the current directory?"
+	read response
+
+	if [[ $response -gt $count_of_files ]]
+	then	
+		incorrect="too high"
+	elif [[ $response -lt $count_of_files ]]
+	then
+		incorrect="too low"
+	else
+		incorrect="correct"
+		action="Congratulations!"
+	fi
+	echo "You entered: $response, which is $incorrect! $action"
+done
+
