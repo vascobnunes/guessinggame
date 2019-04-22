1c73d18920e6f5cc74a9552854e58c10165c4e91
vascobnunes <vascobnunes@gmail.com>
2019-04-22 19:06:45 +0200

    working version

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-count_of_files=$(ls | wc -l)
+function evaluate_response {
 action="Please try again!"
+if [[ $1 -gt $2 ]]
+then
+	evaluation="too high"
+elif [[ $1 -lt $2 ]]
+then
+	evaluation="too low"
+else
+	evaluation="correct"
+	action="Congratulations!"
+fi
+echo "$evaluation! $action" 
+}
 
+count_of_files=$(ls | wc -l)
 while [[ $count_of_files -ne $response ]]
 do
 	echo "Type how many files you guess are in the current directory?"
 	read response
-
-	if [[ $response -gt $count_of_files ]]
-	then	
-		incorrect="too high"
-	elif [[ $response -lt $count_of_files ]]
-	then
-		incorrect="too low"
-	else
-		incorrect="correct"
-		action="Congratulations!"
-	fi
-	echo "You entered: $response, which is $incorrect! $action"
+	echo "You entered: $response, which is $(evaluate_response $response $count_of_files)"
 done
 
